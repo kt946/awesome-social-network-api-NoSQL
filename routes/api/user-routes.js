9fbc059a47b1382086eb8cb86ee5cfbf45759542
KT 2022 <103476893+kt946@users.noreply.github.com>
2022-09-01 17:53:27 -0400

    updated user friends routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,20 +4,28 @@ const {
   getUserById,
   createUser,
   updateUser,
-  deleteUser
+  deleteUser,
+  addFriend,
+  removeFriend
 } = require('../../controllers/user-controller');
 
-// Set up GET all and POST at /api/users
+// /api/users
 router
   .route('/')
   .get(getAllUser)
   .post(createUser);
 
-// Set up GET one, PUT, and DELETE at /api/users/:id
+// /api/users/:userId
 router
-  .route('/:id')
+  .route('/:userId')
   .get(getUserById)
   .put(updateUser)
   .delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router
+  .route('/:userId/friends/:friendId')
+  .post(addFriend)
+  .delete(removeFriend);
 
 module.exports = router;
